05130085321a8a270ad93a8a999e7ddeb23fd474
Wolfara <58333086+Wolfara@users.noreply.github.com>
2019-12-02 13:37:57 +0700

    dsfad

---
+++ Assets/Scripts/DeathByLR.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathByLR : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider col)
+    {
+        if (col.tag == "Player")
+        {
+            col.GetComponent<Move>().lifes--;
+            col.GetComponent<Move>().deathsByLR++;
+            col.GetComponent<Move>().speed = 0;
+            Destroy(gameObject);
+        }
+    }
+}
