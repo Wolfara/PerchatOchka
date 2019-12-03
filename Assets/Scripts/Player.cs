a4be12fddf7ebf4b03dcbcb7caca8a8a459ebc35
Wolfara <58333086+Wolfara@users.noreply.github.com>
2019-12-03 09:38:17 +0700

    Generate blocks

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed;
+    float maxSpeed;
+    float minSpeed;
+    Rigidbody rb;
+    int path;
+    bool prisel;
+    [HideInInspector]
+    public int lifes;
+    [HideInInspector]
+    public int deathsByLR = 0;
+    [HideInInspector]
+    public int deathsByUp = 0;
+    [HideInInspector]
+    public int deathsByDown = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        lifes = 3;
+        minSpeed = 2;
+        speed = minSpeed;
+        path = 2;
+        maxSpeed = 20;
+        rb = gameObject.GetComponent<Rigidbody>();
+        prisel = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 forceUp = new Vector3();
+        transform.position -= new Vector3(speed, 0, 0) * Time.deltaTime;
+        if (speed < maxSpeed)
+            speed += 0.1f * Time.deltaTime;
+        forceUp += transform.up * 7;
+        if (Input.GetKeyDown(KeyCode.A) && path > 1)
+        {
+            transform.position -= new Vector3(0, 0, 2);
+            path--;
+        }
+        if (Input.GetKeyDown(KeyCode.D) && path < 3)
+        {
+            transform.position += new Vector3(0, 0, 2);
+            path++;
+        }
+        if (Input.GetKeyDown(KeyCode.S) && !prisel)
+        {
+            GetComponent<BoxCollider>().size = new Vector3(1, 0.5f, 1);
+            GetComponent<CapsuleCollider>().height = 0.5f;
+            prisel = true;
+        }
+        else if (Input.GetKeyDown(KeyCode.S) && prisel)
+        {
+            GetComponent<BoxCollider>().size = new Vector3(1, 1, 1);
+            GetComponent<CapsuleCollider>().height = 1.1f;
+            transform.position += new Vector3(0, 0.3f, 0);
+            prisel = false;
+        }
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            rb.AddForce(forceUp, ForceMode.Impulse);
+        }
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(other, other);
+        if (other.tag == "ObstLR")
+        {
+            Debug.Log("Life-");
+            speed = minSpeed;
+            lifes--;
+            deathsByLR++;
+            Destroy(other);
+        }
+        if (other.tag == "ObstUp")
+        {
+            Debug.Log("Life-");
+            lifes--;
+            deathsByUp++;
+            Destroy(other);
+        }
+    }
+
+}
--- Assets/Scripts/SpawnObst.cs
+++ Assets/Scripts/SpawnObst.cs
@@ -4,15 +4,47 @@ using UnityEngine;
 
 public class SpawnObst : MonoBehaviour
 {
+    int deathsByUp, deathsByDown, deathsByLR;
+    public GameObject player;
+    int obstaclesGroupNumber;
+    public GameObject[] obstaclesGroup;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        deathsByDown = player.GetComponent<Player>().deathsByDown;
+        deathsByLR = player.GetComponent<Player>().deathsByLR;
+        deathsByUp = player.GetComponent<Player>().deathsByUp;
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            //if (deathsByDown >= 2 && deathsByDown > deathsByLR && deathsByDown > deathsByUp)
+            //{
+            //    obstaclesGroupNumber = 0;
+            //}
+            //else if (deathsByUp >= 2 && deathsByUp > deathsByLR && deathsByUp > deathsByDown)
+            //{
+            //    obstaclesGroupNumber = 1;
+            //}
+            //else if (deathsByLR >= 2 && deathsByLR > deathsByDown && deathsByLR > deathsByUp)
+            //{
+            //    obstaclesGroupNumber = 2;
+            //}
+            //else
+            
+                obstaclesGroupNumber = Random.Range(4, 6);
+            
+            Vector3 vec = new Vector3(player.transform.position.x, player.transform.position.y, 0);
+            Instantiate(obstaclesGroup[obstaclesGroupNumber], vec, obstaclesGroup[obstaclesGroupNumber].transform.rotation);
+            Destroy(gameObject);
+
+        }
     }
 }
